75573a22e4af72498d96381be430406680a483a2
Sanchir <emgushovs@mail.ru>
2023-03-03 18:14:00 +0300

    back first step

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import {Module} from "@nestjs/common";
+
+
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
+
+
+@Module({
+    controllers:[],
+    providers:[],
+    imports:[
+        ConfigModule.forRoot({
+            envFilePath:'env'
+        }),
+        SequelizeModule.forRoot({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'sasha-shop',
+        password: 'sanchirgarik01',
+        database: 'postgres',
+        models: [],
+    }),
+        UsersModule
+    ]
+})
+
+export class AppModule{}
